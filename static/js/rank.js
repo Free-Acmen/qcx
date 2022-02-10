bdc032590f572aed2fd3dbd5f5cb5229d8257246
freeacmen <lmllsq@126.com>
2022-02-10 11:12:43 +0800

    学校代码调整

--- static/js/rank.js
+++ static/js/rank.js
@@ -1,12 +1,12 @@
 var codeObj={
-  '1-10002':{name:'民大',code1:'./static/images/1-10002/销售码.jpg', code2:'./static/images/1-10002/复试群.jpg'},
   '1-10032':{name:'北语',code1:'./static/images/1-10032/销售码.jpg', code2:'./static/images/1-10032/复试群.jpg'},
   '1-10036':{name:'贸大',code1:'./static/images/1-10036/销售码.jpg', code2:'./static/images/1-10036/复试群.jpg'},
   '1-10038':{name:'首经贸',code1:'./static/images/1-10038/销售码.jpg', code2:'./static/images/1-10038/复试群.jpg'},
+  '1-10052':{name:'民大',code1:'./static/images/1-10052/销售码.jpg', code2:'./static/images/1-10052/复试群.jpg'},
   '2-10068':{name:'天外',code1:'./static/images/2-10068/销售码.jpg', code2:'./static/images/2-10068/复试群.jpg'},
   '2-10070':{name:'天财',code1:'./static/images/2-10070/销售码.jpg', code2:'./static/images/2-10070/复试群.jpg'},
   '15-10423':{name:'海大',code1:'./static/images/15-10423/销售码.png', code2:'./static/images/15-10423/复试群.jpg'},
-  '15-10456':{name:'山大',code1:'./static/images/15-10456/销售码.png', code2:'./static/images/15-10456/复试群.jpg'},
+  '15-10422':{name:'山大',code1:'./static/images/15-10422/销售码.png', code2:'./static/images/15-10422/复试群.jpg'},
   '15-11065':{name:'青大',code1:'./static/images/15-11065/销售码.jpg', code2:'./static/images/15-11065/复试群.jpg'},
   '19-10574':{name:'华师',code1:'./static/images/19-10574/销售码.jpg', code2:'./static/images/19-10574/复试群.jpg'},
   '25-10673':{name:'云大',code1:'./static/images/25-10673/销售码.png', code2:'./static/images/25-10673/复试群.jpg'},
@@ -28,26 +28,27 @@ new Vue({
       loading: false,
       finished: false,
       code1:'./static/images/other/销售码.jpg',
-      code2:'./static/images/other/复试群.jpg'
+      code2:'./static/images/other/复试群.jpg',
+      Content:''
     }
   },
   created() {
-      this.uinfo = Cache.get('uinfo');
-      if (!this.uinfo) {
-        window.location.href = './index.html'
-        return
-      }
+    this.uinfo = Cache.get('uinfo');
+    if (!this.uinfo) {
+      window.location.href = './index.html'
+      return
+    }
     this.height = window.innerHeight + 'px';
     this.initData()
-    },
-    mounted() {
-      var that = this
-        setTimeout(function() {
-            // var listHeight = window.innerHeight - $(".grade-info").outerHeight() - $(".footer").outerHeight() - $(".tips").outerHeight() - $(".rank-info").outerHeight() - $(".remark").outerHeight();
-            // this.listHeight = listHeight - 38 + 'px';
-            that.footHeight = $(".footer").outerHeight() + 10 + 'px'
-        }, 100);
-    },
+  },
+  mounted() {
+    var that = this
+    setTimeout(function() {
+        // var listHeight = window.innerHeight - $(".grade-info").outerHeight() - $(".footer").outerHeight() - $(".tips").outerHeight() - $(".rank-info").outerHeight() - $(".remark").outerHeight();
+        // this.listHeight = listHeight - 38 + 'px';
+        that.footHeight = $(".footer").outerHeight() + 10 + 'px'
+    }, 100);
+  },
   methods: {
     initData: function(){
       var that = this
@@ -62,25 +63,26 @@ new Vue({
           that.code1=codeObj['other'].code1
           that.code2=codeObj['other'].code2
         }
-
+        that.getContent()
         Cache.set("rank", res.data);
       })
     },
-      onLoad() {
-          var that = this
-          this.ajax('getRankList', this.uinfo, 1).then(function (res) {
-             
-              that.loading = false;
-              that.list = res.data
-              
-              that.finished = true;
-
-          })
-      },
-      updateHandle: function () {
-          this.update = true
-          // window.location.href = 'https://mp.weixin.qq.com/s/ahTUGEGzDIWD_axgh6EdLw'
-      },
+    onLoad() {
+      var that = this
+      this.ajax('getRankList', this.uinfo, 1).then(function (res) {
+        that.loading = false;
+        that.list = res.data
+        
+        that.finished = true;
+      })
+    },
+    getContent: function(){
+      this.Content="<p>这是历史成绩</p>\n<p>录取人数256人，录取分数386</p>\n<p><img class=\"wscnph\" src=\"//adm.kaoyanxiao.com/dat/Uploads/HYJY/e21c29885d3c42fc83287bfed81045f0.jpg\" width=\"300\" /></p>"
+    },
+    updateHandle: function () {
+        this.update = true
+        // window.location.href = 'https://mp.weixin.qq.com/s/ahTUGEGzDIWD_axgh6EdLw'
+    },
     communicationHandle: function(){
       this.communication = true
     },
@@ -98,7 +100,7 @@ new Vue({
       });
     },
     share: function(){
-        this.shareFlag = true;
+      this.shareFlag = true;
     }
   }
 });
